b16ffe1a590ced5f98226bc0a3a9690b50e880fe
David Kimmel <davidwkimmel@gmail.com>
2021-01-09 12:16:23 -0800

    fixing set pixel value

--- cpp/impl/screens/screen_circle.cpp
+++ cpp/impl/screens/screen_circle.cpp
@@ -19,20 +19,24 @@ screen_circle::~screen_circle() {}
 void screen_circle::draw(bitmap_image & img)
 {
     std::cout << "screen_circle::draw()\n";
+
+    const double x_center {5.0};
+    const double y_center {5.0};
+    const double radius {5.0};
+    const uint8_t pixel_val {0}; // black
+
     // Display circle in upper left
     for(size_t x = 0; x < 10; ++x)
     {
         for(size_t y = 0; y < 10; ++y)
         {
-            const double x_center {5.0};
-            const double y_center {5.0};
-            const double radius {5.0};
 
-            if(radius > sqrt(
-                    pow(x_center - x, 2) + pow(y_center - y, 2)
-            ))
+            if(radius > sqrt
+                ( pow(x_center - x, 2)
+                + pow(y_center - y, 2)
+                ))
             {
-                img.set(x, y, 1);
+                img.set(x, y, pixel_val);
             }
         }
     }
--- cpp/impl/screens/screen_square.cpp
+++ cpp/impl/screens/screen_square.cpp
@@ -14,12 +14,13 @@ screen_square::~screen_square() {}
 void screen_square::draw(bitmap_image & img)
 {
     std::cout << "screen_square::draw()\n";
+    const uint8_t pixel_val {0}; // black
     // Display box in lower right
     for(size_t x = img.image_width_pixels()-10; x < img.image_width_pixels(); ++x)
     {
         for(size_t y = img.image_height_pixels()-10; y < img.image_height_pixels(); ++y)
         {
-            img.set(x, y, 1);
+            img.set(x, y, pixel_val);
         }
     }
 }
