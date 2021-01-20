545c14aac95f65445ea80849b30a3d065f76dbb1
David Kimmel <davidwkimmel@gmail.com>
2021-01-19 17:11:09 -0800

    current time widget

--- cpp/impl/screens/screen_currently_playing.cpp
+++ cpp/impl/screens/screen_currently_playing.cpp
@@ -13,7 +13,7 @@ screen_currently_playing::screen_currently_playing
     ( std::shared_ptr<lms_client> lms_client
     ) : m_lms_client(lms_client)
       , m_widget_currently_playing(new widget_string("Unknown", 10, 20, 0, 255))
-      , m_widget_current_time(new widget_current_time())
+      , m_widget_current_time(new widget_current_time(0, 0, 0, 255))
 {
     m_running = true;
     m_thread = std::thread([&]()
@@ -61,22 +61,24 @@ void screen_currently_playing::draw(bitmap_image & img)
 
     {
         // Draw time block
-        const int time_block_height {40};
-        const int time_block_width {80};
+//        const int time_block_height {40};
+//        const int time_block_width {80};
+//
+//        for(int i = 0; i < time_block_height; ++i)
+//        {
+//            drawing::Paint_DrawLine
+//                ( img
+//                , img.image_width_pixels() - time_block_width, img.image_height_pixels() - i
+//                , img.image_width_pixels() - 0, img.image_height_pixels() - i
+//                , drawing::d_BLACK
+//                , drawing::d_DOT_PIXEL_DFT
+//                , drawing::d_LINE_STYLE_SOLID
+//                );
+//        }
 
-        for(int i = 0; i < time_block_height; ++i)
-        {
-            drawing::Paint_DrawLine
-                ( img
-                , img.image_width_pixels() - time_block_width, img.image_height_pixels() - i
-                , img.image_width_pixels(), img.image_height_pixels() - i
-                , drawing::d_BLACK
-                , drawing::d_DOT_PIXEL_DFT
-                , drawing::d_LINE_STYLE_SOLID
-                );
-        }
-
-
+        // Move clock to correct position
+        m_widget_current_time->x() = img.image_width_pixels() - 50;
+        m_widget_current_time->y() = img.image_height_pixels() - 20;
         m_widget_current_time->draw(img);
     }
 
